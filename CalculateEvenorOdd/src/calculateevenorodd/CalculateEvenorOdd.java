360f7e7c7b081a79426c7de4f8b700ce93c7f3dc
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-04-15 18:57:11 +0600

    Update CalculateEvenorOdd.java

--- CalculateEvenorOdd/src/calculateevenorodd/CalculateEvenorOdd.java
+++ CalculateEvenorOdd/src/calculateevenorodd/CalculateEvenorOdd.java
@@ -1,18 +1,33 @@
 package calculateevenorodd;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class CalculateEvenorOdd {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter number");
-        int evenOdd = input.nextInt();
-        if (evenOdd % 2 == 0) {
-            System.out.println("This Number is Even");
-        } else {
-            System.out.println("This Number is Odd");
-        }
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter number");
+//        int evenOdd = input.nextInt();
+//        if (evenOdd % 2 == 0) {
+//            System.out.println("This Number is Even");
+//        } else {
+//            System.out.println("This Number is Odd");
+//        }
+//int ch= -10;
+//switch(ch){
+//case 1 : ch++; break;
+//case 2 : ch++; break;
+//default: ch++;
+//case 3 : ch++; break;
+//case 4 : ch++;
+//}
+//System.out.println(ch);
+
+int i = 1;
+do{
+i--;
+} while (i > 2);
+System.out.println(i);
 
     }
 
