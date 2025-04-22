5cafeea0c9987f524316542a70646e802ba3c0e0
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-04-23 01:26:04 +0600

    class add

--- NewClassObject/src/newclassobject/NewClassObject.java
+++ NewClassObject/src/newclassobject/NewClassObject.java
@@ -1,25 +1,40 @@
-
 package newclassobject;
 
 import newclassobject.student.StudentNew;
 
-
 public class NewClassObject {
 
-    
     public static void main(String[] args) {
-        StudentNew s=new StudentNew();
+//        StudentNew s=new StudentNew();
+
+//        s.setName("Atiq");
+//        s.setEmail("atiq@gmail.com");
+//        s.setMarks(50);
+//        s.setId(12345);
+//        s.setPasword(67865734);
+//        s.setAddress("Ajimpur, Dhaka");
+//        s.setInstituteName("Dhaka Univarsity");
+//        s.setNumber("01722652595");
+////        
+//        System.out.println(s);
+//        StudentNew reja = new StudentNew("Reja", 60);
+//        System.out.println(reja.getMarks());
+//
+//        System.out.println(reja.getResult());
         
-        s.setName("Atiq");
-        s.setEmail("atiq@gmail.com");
-        s.setMarks(50);
-        s.setId(12345);
-        s.setPasword(67865734);
-        s.setAddress("Ajimpur, Dhaka");
-        s.setInstituteName("Dhaka Univarsity");
-        s.setNumber("01722652595");
-        
-        System.out.println(s);
+
+//public static void ar jonno.
+StudentNew lowerMethod=new StudentNew(); 
+
+lowerMethod.setName("Eiasin");
+lowerMethod.setId(1236564);
+
+        printStudentDetails(lowerMethod);
     }
-    
+
+    public static void printStudentDetails(StudentNew sn) {
+
+        System.out.println("Name: " + sn.getName() + "\n" + "Id: " + sn.getId());
+    }
+
 }
