4d898c912b4f48f3c53d622531e984bdd85e55f2
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-05-21 22:34:04 +0600

    add pont of sale

---
+++ ThreatTest/src/threattest/ThreatTest.java
@@ -0,0 +1,18 @@
+
+package threattest;
+
+
+public class ThreatTest {
+
+    public static void main(String[] args) {
+        
+   class multithreaded_programing {
+public static void main(String args[]) {
+Thread t = Thread.currentThread();
+System.out.println(t);
+}
+}
+
+    }
+    
+}
